0e3eb9bb4fea322a5af6c97d1620cca16db8ef9b
Abel S Mathew <abelsmathew228@gmail.com>
2026-01-27 15:48:45 +0530

    Updated path where law summaries are saved

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     law_data = fetch_law_summary(law_name)
 
     if law_data:
-        save_path = os.path.join("data", "laws_json", f"{law_name}_context.json")
+        save_path = os.path.join("data", "processed" ,"laws_json", f"{law_name}_context.json")
         os.makedirs(os.path.dirname(save_path), exist_ok=True)
         with open(save_path, "w") as f:
             json.dump(law_data, f, indent=4)
